e1ea8bee18f63b6daafb163cc03f2d3f6a82fd54
Timework <timework.bibo@gmail.com>
2020-10-03 03:10:48 -0500

    Updates

---
+++ precache-manifest.63f8722f25e0bea7703cae0087935a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03eb2499f1166aa647c745a0bdd9e52",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "145722ef3496143b0049",
+    "url": "/cv-project/static/css/main.24d4eb47.chunk.css"
+  },
+  {
+    "revision": "df3b2fb21cdbce425b89",
+    "url": "/cv-project/static/js/2.fb43bcc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-project/static/js/2.fb43bcc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "145722ef3496143b0049",
+    "url": "/cv-project/static/js/main.06b62c66.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  }
+]);
